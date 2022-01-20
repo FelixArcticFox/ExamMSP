38d2d3869156b106c8bab161f7ea926eb502f818
Felix <felix018237@gmail.com>
2022-01-20 15:49:49 +0600

    My names Dastan

---
+++ ExamPO/ExamPO/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamPO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string writePath = @"E:\testCommit.txt";
+            string hello = "Hello World!";
+            string text = "My name is Dastan Urazov"; 
+            Random random = new Random();
+            int number = random.Next(0,100);
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(text);
+                    sw.WriteLine(hello);
+                    sw.WriteLine(number);
+                    Console.WriteLine(hello);
+                    Console.WriteLine(number);
+                }
+                Console.WriteLine("Запись выполнена");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
